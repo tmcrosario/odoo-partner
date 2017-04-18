7da5c4e8f19bf141614ac1449af1b6401972b92f
Liso Gallo <liso@riseup.net>
2017-04-18 09:29:15 -0300

    [ADD] partner_tmc addon

---
+++ partner_tmc/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Customizations for TMC Rosario',
+    'summary': 'Add or hide some information related to partner.',
+    'version': '10.0.1.0.0',
+    'category': 'Contacts',
+    'website': 'https://www.tmcrosario.gob.ar',
+    'author': 'Tribunal Municipal de Cuentas - Municipalidad de Rosario',
+    'license': 'AGPL-3',
+    'depends': [
+        'partner_contact_birthdate',
+        'partner_firstname',
+        'partner_contact_gender',
+        'partner_street_number',
+        'partner_fiscal',
+        'partner_municipal'
+    ],
+    'data': [
+        'views/partner.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'qweb': [],
+}
