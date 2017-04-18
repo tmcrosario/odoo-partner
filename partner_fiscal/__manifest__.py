ac62d79a8548107395ed8467973bb9f403597233
Liso Gallo <liso@riseup.net>
2017-04-18 09:29:03 -0300

    [ADD] partner_fiscal addon

---
+++ partner_fiscal/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'AFIP Partner Extension',
+    'summary': 'Add fiscal information to partner.',
+    'version': '10.0.1.0.0',
+    'category': 'Contacts',
+    'website': 'https://www.tmcrosario.gob.ar',
+    'author': 'Tribunal Municipal de Cuentas - Municipalidad de Rosario',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [
+        'views/partner.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'qweb': [],
+}
---
+++ partner_fiscal/models/partner.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+import re
+
+
+class Partner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    __check_cuit_re = re.compile(
+        r'([0-9]{2}\-[0-9]{8}\-[0-9]{1})$', re.IGNORECASE)
+
+    def _validate_cuit(self):
+        if len(self.cuit) != 13 or self.cuit[2] != "-" or self.cuit[11] != "-":
+            return False
+
+        base = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+
+        cuit = self.cuit.replace("-", "")
+
+        aux = 0
+        for i in xrange(10):
+            aux += int(cuit[i]) * base[i]
+
+        aux = 11 - (aux - (int(aux / 11) * 11))
+
+        if aux == 11:
+            aux = 0
+        if aux == 10:
+            aux = 9
+
+        return aux == int(cuit[10])
+
+    @api.constrains('cuit')
+    def _check_cuit(self):
+        if self.cuit:
+            if self.__check_cuit_re.match(self.cuit) and self._validate_cuit():
+                return True
+            else:
+                raise Warning(_('Invalid CUIT'), _(
+                    'Valid format: XX-XXXXXXXX-X'))
+
+    cuit = fields.Char(
+        string='CUIT',
+        size=13,
+        help='Format: XX-XXXXXXXX-X'
+    )
+
+    fiscal_situation = fields.Selection([
+        ('monotributista', 'Monotributista'),
+        ('responsable_inscripto', 'Responsable Inscripto')]
+    )
