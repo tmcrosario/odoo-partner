5e172ce285f63c9b75a40d697c09aed082d83ff6
Lisandro Gallo <4266083+lisandrogallo@users.noreply.github.com>
2021-04-26 16:54:51 -0300

    [FIX] Bugfix py2 xrange to py3 range

--- partner_fiscal/models/partner.py
+++ partner_fiscal/models/partner.py
@@ -1,24 +1,25 @@
 import re
 
-from odoo import models, fields, api, _, exceptions
+from odoo import _, api, exceptions, fields, models
 
 
 class Partner(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
+    _name = "res.partner"
+    _inherit = "res.partner"
 
-    __check_cuit_re = re.compile(r'([0-9]{2}\-[0-9]{8}\-[0-9]{1})$',
-                                 re.IGNORECASE)
+    __check_cuit_re = re.compile(
+        r"([0-9]{2}\-[0-9]{8}\-[0-9]{1})$", re.IGNORECASE
+    )
 
     def _validate_cuit(self):
         raw_cuit = self.cuit
-        if len(raw_cuit) != 13 or raw_cuit[2] != '-' or raw_cuit[11] != '-':
+        if len(raw_cuit) != 13 or raw_cuit[2] != "-" or raw_cuit[11] != "-":
             return False
         else:
             base = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
-            cuit = raw_cuit.replace('-', '')
+            cuit = raw_cuit.replace("-", "")
             aux = 0
-            for i in xrange(10):
+            for i in range(10):
                 aux += int(cuit[i]) * base[i]
             aux = 11 - (aux - (int(aux / 11) * 11))
             if aux == 11:
@@ -28,16 +29,21 @@ class Partner(models.Model):
 
             return aux == int(cuit[10])
 
-    @api.constrains('cuit')
+    @api.constrains("cuit")
     def _check_cuit(self):
         if self.cuit:
-            if not (self.__check_cuit_re.match(self.cuit)
-                    and self._validate_cuit()):
+            if not (
+                self.__check_cuit_re.match(self.cuit) and self._validate_cuit()
+            ):
                 raise exceptions.UserError(
-                    _('Invalid CUIT. Valid format: XX-XXXXXXXX-X'))
+                    _("Invalid CUIT. Valid format: XX-XXXXXXXX-X")
+                )
 
-    cuit = fields.Char(string='CUIT', size=13, help='Format: XX-XXXXXXXX-X')
+    cuit = fields.Char(string="CUIT", size=13, help="Format: XX-XXXXXXXX-X")
 
-    fiscal_situation = fields.Selection([('monotributista', 'Monotributista'),
-                                         ('responsable_inscripto',
-                                          'Responsable Inscripto')])
+    fiscal_situation = fields.Selection(
+        [
+            ("monotributista", "Monotributista"),
+            ("responsable_inscripto", "Responsable Inscripto"),
+        ]
+    )
